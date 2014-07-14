a3118f74f34d4f161b3c8d171a6dc2097153208f
root <root@lamp.tu.lan>
2014-07-14 15:54:06 +1000

    second comment

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<?php 
+
+echo "<h1> PARKS protoype website goes here...</h1>";
+
+
+?>
+
+<?php echo "<p>attempting to connect to DB: parksdb</p>"; 
+	echo "<p>user: parksdb | password: admin</p>";
+
+?>
+
+<?php 
+// Create connection
+$con=mysqli_connect("localhost","parksdb","admin","parksdb");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else{
+	echo "<p>connection successful</p>";
+}
+?>
+
+</html>
